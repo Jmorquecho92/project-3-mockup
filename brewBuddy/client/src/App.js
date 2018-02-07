43584ede53eb4eeeb93a5639c8aeb873425b054d
cpbarrett <cpbarrett@gmail.com>
2018-02-06 20:46:31 -0800

    react fixes

---
+++ brewBuddy/client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+import {Home} from './Home.js'
+import {Find} from './Find.js'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <Home />;
+      </div>
+    );
+  }
+}
+
+export default App;
